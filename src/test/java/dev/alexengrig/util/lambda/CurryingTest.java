bcfa985da0c360882666415d3c6cc1dc6764ee5e
Grig Alex <alexengrigdev@gmail.com>
2022-07-04 21:10:46 +0300

    Add tests for predicates in Currying

--- src/test/java/dev/alexengrig/util/lambda/CurryingTest.java
+++ src/test/java/dev/alexengrig/util/lambda/CurryingTest.java
@@ -348,4 +348,164 @@ class CurryingTest {
 
     }
 
+    @Nested
+    class Predicates {
+
+        boolean odd(String first) {
+            return (Integer.parseInt(first) & 1) != 0;
+        }
+
+        boolean odd2(String first, String second) {
+            return ((Integer.parseInt(first) + Integer.parseInt(second)) & 1) != 0;
+        }
+
+        boolean odd3(String first, String second, String third) {
+            return ((Integer.parseInt(first) + Integer.parseInt(second) + Integer.parseInt(third)) & 1) != 0;
+        }
+
+        @Test
+        void testLeft2() {
+            var lambda = Currying.left2(this::odd2);
+            var lambda1 = lambda.apply(ONE);
+            boolean result = lambda1.test(TWO);
+            Assertions.assertTrue(result, "12");
+        }
+
+        @Test
+        void testLeft2WithArg() {
+            var lambda = Currying.left2(this::odd2, ONE);
+            boolean result = lambda.test(TWO);
+            Assertions.assertTrue(result, "12");
+        }
+
+        @Test
+        void testLeft3() {
+            var lambda = Currying.left3(this::odd3);
+            var lambda1 = lambda.apply(ONE);
+            var lambda2 = lambda1.apply(TWO);
+            boolean result = lambda2.test(THREE);
+            Assertions.assertFalse(result, "123");
+        }
+
+        @Test
+        void testLeft3WithArg() {
+            var lambda = Currying.left3(this::odd3, ONE);
+            var lambda1 = lambda.apply(TWO);
+            boolean result = lambda1.test(THREE);
+            Assertions.assertFalse(result, "123");
+        }
+
+        @Test
+        void testLeft3WithArgs() {
+            var lambda = Currying.left3(this::odd3, ONE, TWO);
+            boolean result = lambda.test(THREE);
+            Assertions.assertFalse(result, "123");
+        }
+
+        @Test
+        void testLeftMiddle3() {
+            var lambda = Currying.leftMiddle3(this::odd3, ONE);
+            var lambda1 = lambda.apply(TWO);
+            boolean result = lambda1.test(THREE);
+            Assertions.assertFalse(result, "213");
+        }
+
+        @Test
+        void testMiddle3() {
+            var lambda = Currying.middle3(this::odd3, ONE, TWO);
+            boolean result = lambda.test(THREE);
+            Assertions.assertFalse(result, "132");
+        }
+
+        @Test
+        void testRightMiddle3() {
+            var lambda = Currying.rightMiddle3(this::odd3, ONE);
+            var lambda1 = lambda.apply(TWO);
+            boolean result = lambda1.test(THREE);
+            Assertions.assertFalse(result, "312");
+        }
+
+        @Test
+        void testRight2() {
+            var lambda = Currying.right2(this::odd2);
+            var lambda1 = lambda.apply(ONE);
+            boolean result = lambda1.test(TWO);
+            Assertions.assertTrue(result, "21");
+        }
+
+        @Test
+        void testRight2WithArg() {
+            var lambda = Currying.right2(this::odd2, ONE);
+            boolean result = lambda.test(TWO);
+            Assertions.assertTrue(result, "21");
+        }
+
+        @Test
+        void testRight3() {
+            var lambda = Currying.right3(this::odd3);
+            var lambda1 = lambda.apply(ONE);
+            var lambda2 = lambda1.apply(TWO);
+            boolean result = lambda2.test(THREE);
+            Assertions.assertFalse(result, "321");
+        }
+
+        @Test
+        void testRight3WithArg() {
+            var lambda = Currying.right3(this::odd3, ONE);
+            var lambda1 = lambda.apply(TWO);
+            boolean result = lambda1.test(THREE);
+            Assertions.assertFalse(result, "321");
+        }
+
+        @Test
+        void testRight3WithArgs() {
+            var lambda = Currying.right3(this::odd3, ONE, TWO);
+            boolean result = lambda.test(THREE);
+            Assertions.assertFalse(result, "312");
+        }
+
+        @Test
+        void testBiLeft3() {
+            var lambda = Currying.biLeft3(this::odd3, ONE);
+            boolean result = lambda.test(TWO, THREE);
+            Assertions.assertFalse(result, "123");
+        }
+
+        @Test
+        void testBiMiddle3() {
+            var lambda = Currying.biMiddle3(this::odd3, ONE);
+            boolean result = lambda.test(TWO, THREE);
+            Assertions.assertFalse(result, "213");
+        }
+
+        @Test
+        void testBiRight3() {
+            var lambda = Currying.biRight3(this::odd3, ONE);
+            boolean result = lambda.test(TWO, THREE);
+            Assertions.assertFalse(result, "231");
+        }
+
+        @Test
+        void testAll() {
+            var lambda = Currying.all(this::odd, ONE);
+            boolean result = lambda.getAsBoolean();
+            Assertions.assertTrue(result, "1");
+        }
+
+        @Test
+        void testAll2() {
+            var lambda = Currying.all2(this::odd2, ONE, TWO);
+            boolean result = lambda.getAsBoolean();
+            Assertions.assertTrue(result, "12");
+        }
+
+        @Test
+        void testAll3() {
+            var lambda = Currying.all3(this::odd3, ONE, TWO, THREE);
+            boolean result = lambda.getAsBoolean();
+            Assertions.assertFalse(result, "123");
+        }
+
+    }
+
 }
